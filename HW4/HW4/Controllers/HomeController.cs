c8df755b716220f4784696e9299de07272d584b1
Kyle Walsh <kwalsh15@mail.wou.edu>
2016-11-02 18:47:04 -0700

    Page 1 completed

---
+++ HW4/HW4/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW4.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Page1()
+        {
+            if (!String.IsNullOrEmpty(Request.QueryString["food"]))
+            {
+                string food = "My favorite food is also " + Request.QueryString["food"];
+                ViewBag.foodData = food;
+            }
+                        
+            return View();
+        }
+
+
+    }
+}
