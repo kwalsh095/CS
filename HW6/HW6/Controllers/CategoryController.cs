e83ae0e46bc05a9c33a51333e3435974d55880d7
Kyle Walsh <kwalsh15@mail.wou.edu>
2016-11-13 00:23:58 -0800

    Beta Build, incomplete

---
+++ HW6/HW6/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using HW6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW6.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private AdventureContext db = new AdventureContext();
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View(db.ProductCategories.ToList());
+        }
+
+        public ActionResult Sub(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var subs = db.ProductSubcategories.Where(p => p.ProductCategoryID == (int)id).ToList();
+
+            return View(subs);
+        }
+    }
+}
---
+++ HW6/HW6/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW6.Controllers
+{
+    public class ProductController : Controller
+    {
+        private AdventureContext db = new AdventureContext();
+
+        // GET: Product
+        public ActionResult List(int? id)
+        {
+            if (id == null)
+            {
+                return View(db.Products.ToList());
+            }
+
+            return View(db.Products.Where(p => p.ProductSubcategoryID == id).ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            var product = db.Products.Find(id);
+
+            if (product == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            return View(product);
+        }
+    }
+}
